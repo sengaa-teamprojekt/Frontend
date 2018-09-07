4d163789702d1a4fc9c0ecbc47a852a0f2e539d4
tatjanagraf <tatjana.graf@yahoo.com>
2018-09-07 10:34:46 +0200

    Changes for Checkboxes and Radiobuttons

--- js_controller/general_controller.js
+++ js_controller/general_controller.js
@@ -4,3 +4,16 @@ $('#header_dd_deal').dropdown();
 $('#header_dd_event').dropdown();
 $('#header_dd_account').dropdown();
 $('#frequency_dd').dropdown();
+
+$('#cb_preiseins').change(function() {
+    $('#preiseins').attr('disabled',!this.checked)
+});
+$('#cb_preiszwei').change(function() {
+    $('#preiszwei').attr('disabled',!this.checked)
+});
+$('#cb_preisdrei').change(function() {
+    $('#preisdrei').attr('disabled',!this.checked)
+});
+$('#cb_preisalle').change(function() {
+    $('#preisalle').attr('disabled',!this.checked)
+});
